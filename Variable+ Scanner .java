8b6b47aac2051e1d98c995ed5e7155b1628a2289
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-04 19:01:39 +0530

    Create Variable+ Scanner .java

---
+++ Variable+ Scanner .java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    //System.out.print("Hi I am there!");
+    // String user_name = "Jack@Engineer";
+    // int password = 12345667;
+
+    // System.out.println(user_name);
+    // System.out.println(password);
+
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    System.out.println("Your Input");
+    System.out.println(n);
+    }
+}
