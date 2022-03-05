d02991e92d032a38b946a92d11c860f956b156a3
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-05 19:02:49 +0530

    Create Sorry_Shaktiman.java

---
+++ Sorry_Shaktiman.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter number of times you want to print your favorite line");
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    for(int i=1; i<=n;i++){
+      System.out.println(i);
+      System.out.println("Sorry ShaktiMan");
+    }
+  }
+}
